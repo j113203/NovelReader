2eb89796b63165ae60bf2240885c272b39de4602
j113203 <j113203@gmail.com>
2015-08-13 23:44:44 +0800

    //hjwzw method update

--- ASP.net/NovelReader/novel.ashx.vb
+++ ASP.net/NovelReader/novel.ashx.vb
@@ -11,11 +11,11 @@ Public Class index
             .Response.Write("<html><head><meta charset=""UTF-8""><title>Novel</title></head><body>")
             .Response.Flush()
             If String.IsNullOrEmpty(.Request.QueryString("n")) Then
-                public_.about(.Request.Url.Scheme + "://" + .Request.Url.Authority + .Request.Path)
+                .Response.Write(public_.about(.Request.Url.Scheme + "://" + .Request.Url.Authority + .Request.Path))
             Else
                 If Not Analy(.Request.QueryString("n")) Then
                     .Response.Write("<h1>錯誤信息: 不支援的網站</h1>")
-                    public_.about(.Request.Url.Scheme + "://" + .Request.Url.Authority + .Request.Path)
+                    .Response.Write(public_.about(.Request.Url.Scheme + "://" + .Request.Url.Authority + .Request.Path))
                 End If
             End If
             .Response.Write("<style>body{background-color: #2a2a2a;color: #C0c0c0;padding: 10%;}</style></body></html>")
@@ -25,11 +25,11 @@ Public Class index
     Function Analy(URL As String) As Boolean
         With Novel
             If URL.ToLower.StartsWith("http://tw.hjwzw.com/book/read/") Then
-                Dim a As New hjwzw
+                Dim a As New hjwzw2
                 .Response.Write(a.get_(URL.Substring("http://tw.hjwzw.com/book/read/".Length), public_.about(.Request.Url.Scheme + "://" + .Request.Url.Authority + .Request.Path)))
                 Return True
             ElseIf URL.ToLower.StartsWith("http://t.hjwzw.com/read/") Then
-                Dim a As New hjwzw
+                Dim a As New hjwzw2
                 .Response.Write(a.get_(URL.Substring("http://t.hjwzw.com/Read/".Length), public_.about(.Request.Url.Scheme + "://" + .Request.Url.Authority + .Request.Path)))
                 Return True
             End If
